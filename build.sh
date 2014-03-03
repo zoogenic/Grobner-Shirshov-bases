b343f9163dfbf80dc2e35e02b912d32d4c6c1813
zoogenic <zoogenic@gmail.com>
2014-03-03 17:15:55 +0400

    added c++11 support; added enum for A5 group [2]

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+export CC=/usr/bin/gcc-4.8
+export CXX=/usr/bin/g++-4.8
+
 cmake $@
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,10 @@
 // STL
 #include <iostream>
 
+#include "Groups/a5.h"
+
 int main()
 {
-    std::cout << "test" << std::endl;
+    Groups::A5 v = Groups::A5::a1;
+    std::cout << static_cast<std::underlying_type<Groups::A5>::type>(v) << std::endl;
 }
