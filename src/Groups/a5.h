87022870b528f827c85998dd0a95235a0fe6b08c
zoogenic <zoogenic@gmail.com>
2014-03-03 17:03:38 +0400

    added c++11 support; added enum for A5 group

---
+++ src/Groups/a5.h
@@ -0,0 +1,17 @@
+/**
+ * @file a5.h
+ * @date Mar 3, 2014
+ * @author zoogenic
+ */
+
+#ifndef _GROBNER_SHIRSHOV_BASES__A5_H_
+#define _GROBNER_SHIRSHOV_BASES__A5_H_
+
+namespace Groups
+{
+
+enum class A5 { a0, a1, a2, a3, a4 };
+
+}
+
+#endif /* _GROBNER_SHIRSHOV_BASES__A5_H_ */
